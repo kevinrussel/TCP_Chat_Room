37f8e3942bf2783dbe9ec375d71b111ce769da97
kevin russel <kzone159@gmail.com>
2026-01-04 19:57:06 -0600

    fixing server issue

--- server.py
+++ server.py
@@ -48,6 +48,8 @@ def connect_method():
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 LOCAL_IP_ADDRESS = '127.0.0.1'
 LOCAL_PORT_ADDRESS = 8080
+# This will allow the port for reconnection
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((LOCAL_IP_ADDRESS,LOCAL_PORT_ADDRESS))
 ## How many connections we will support.
 server.listen(5)
