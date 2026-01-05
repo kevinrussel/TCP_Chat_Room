17d31b42a44e9f14b9aa536858af0f1314b06221
kevin russel <kzone159@gmail.com>
2026-01-04 22:30:46 -0600

    more edits

--- client.py
+++ client.py
@@ -6,9 +6,9 @@ write = True
 stop = threading.Event()
 
 def write_message(stop):
-    print("in write message")
+    
     while not stop.is_set():
-        
+            
         message = input()
         message = message.encode('utf-8')
         client.sendall(message)
